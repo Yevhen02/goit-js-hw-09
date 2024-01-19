2983bc6c088bace594ae59f5bc6cb02fad738f64
Yevhen02 <153821695+Yevhen02@users.noreply.github.com>
2024-01-19 18:19:47 +0200

    update

---
+++ src/js/2-form.js
@@ -0,0 +1,48 @@
+// SELECT HTML ELEMENTS TO WORK WITH
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements['email'];
+const messageInput = form.elements['message'];
+
+// SETUP REFERENCE TO LOCAL STORAGE DATA
+document.addEventListener('DOMContentLoaded', () => {
+  const storedLocStorData = localStorage.getItem('feedback-form-state');
+
+  if (storedLocStorData) {
+    const { email, message } = JSON.parse(storedLocStorData);
+    // emailInput.value = email ?? '';
+    emailInput.value = email === '';
+
+    // messageInput.value = message ?? '';
+    messageInput.value = message === '';
+  }
+});
+
+// STORE TO LOCAL STORAGE
+form.addEventListener('input', () => {
+  const currentState = {
+    email: emailInput.value.trim(),
+    message: messageInput.value.trim(),
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
+});
+
+// STORE TO LOCAL STORAGE
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (!emailInput.value.trim() || !messageInput.value.trim()) {
+    console.log('Please fill out input fields.');
+    return;
+  }
+
+  const currentLocStorData = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  console.log(currentLocStorData);
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+});
+
+// SETUP TO EXPORT BY DEFAULT
+export default {};
---
+++ src/main.js
@@ -0,0 +1,2 @@
+import gallery from './js/1-gallery';
+import form from './js/2-form';
